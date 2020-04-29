4573c188dcc80a9688c7cb3dcf173e9f7cf9d9a2
willmorim <pdbawilliam@gmail.com>
2020-04-28 22:53:53 -0300

    configurando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-  <h1>Oi William</h1>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
